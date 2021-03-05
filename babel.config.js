0cabd07a27eea02a5268299f0268b175dc7fa78a
lvyou <376607092@qq.com>
2021-03-05 21:40:22 +0800

    chore: 配置项目路加别名

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@/pages': '.src/pages',
+          '@/utils': '.src/utils',
+          '@/components': '.src/components',
+          '@/assets': '.src/assets',
+        },
+      },
+    ],
+  ],
 };
